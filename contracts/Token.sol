a19180b8cab8a7fa875e2b26363f08e76baec07a
cbucks <cbucks.blockchain@gmail.com>
2019-12-19 16:18:38 +0800

    feat: Updated Token Contract removed auto burn

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.5.3;
 // Deployed To : 0x4d9ee34b7ee0d3cef04e5909c27a266e7eb14712
 // Symbol      : CBUCKS
 // Name        : CRYPTOBUCKS
-// Total Supply: 1,000,000,000 CBUCKS
+// Total Supply: 10,000,000,000 CBUCKS
 // Decimals    : 2
 //
 // (c) By 'ANONYMOUS' With 'CBUCKS' Symbol 2019.
@@ -30,24 +30,24 @@ contract Token is IERC20Token, Whitelist, Pausable {
   string _symbol;
   uint256 _totalSupply;
   uint256 _decimals;
-  uint256 _burnRate;
   uint256 _totalBurned;
 
   constructor () public {
     _name = "CRYPTOBUCKS";
     _symbol = "CBUCKS";
-    _totalSupply = 100000000000;
+    _totalSupply = 1000000000000;
     _decimals = 2;
-    _burnRate = 100;
     _totalBurned = 0;
-    balances[0x06fa453ae3FD4F46f9e0d5a177F9e412e047a8DB] = _totalSupply;
-    emit Transfer(address(this), 0x06fa453ae3FD4F46f9e0d5a177F9e412e047a8DB, _totalSupply);
+    balances[0xE43eBCb96564a6FB3B7A4AbbfD7008b415591b09] = _totalSupply;
+    emit Transfer(address(this), 0xE43eBCb96564a6FB3B7A4AbbfD7008b415591b09, _totalSupply);
   }
 
   mapping(address => uint256) private balances;
   mapping(address => mapping(address => uint256)) private allowed;
+  mapping(address => bool) private burners;
+
   event Burned(address indexed from, uint256 value, uint256 timestamp);
-  event BurnRateChanged(address indexed from, uint256 value, uint256 timestamp);
+  event AssignedBurner(address indexed from, address indexed burner, uint256 timestamp);
 
   function name() external view returns (string memory) {
     return _name;
@@ -77,10 +77,9 @@ contract Token is IERC20Token, Whitelist, Pausable {
     address recipient,
     uint256 amount
     ) external whenNotPaused onlyWhitelisted(msg.sender, recipient) validRecipient(recipient)
-    validAmount(amount) validAddress(recipient) sufficientBalance(msg.sender, amount) returns (bool) {
+    validAmount(amount) validAddress(recipient) returns (bool) {
       balances[msg.sender] = balances[msg.sender].sub(amount);
       balances[recipient] = balances[recipient].add(amount);
-      burn();
       emit Transfer(msg.sender, recipient, amount);
   }
 
@@ -88,7 +87,7 @@ contract Token is IERC20Token, Whitelist, Pausable {
     address spender,
     uint256 amount
     ) external whenNotPaused validAddress(spender) validRecipient(spender)
-    validAmount(amount) sufficientBalance(msg.sender, amount) returns (bool) {
+    validAmount(amount) returns (bool) {
     allowed[msg.sender][spender] = allowed[msg.sender][spender].add(amount);
     emit Approval(msg.sender, spender, amount);
   }
@@ -98,11 +97,10 @@ contract Token is IERC20Token, Whitelist, Pausable {
     address recipient,
     uint256 amount
     ) external whenNotPaused validAddress(recipient) validRecipient(recipient)
-    validAmount(amount) sufficientBalance(msg.sender, amount) returns (bool) {
+    validAmount(amount) returns (bool) {
       require(allowed[sender][msg.sender] >= amount, "Above spender allowance.");
       allowed[sender][msg.sender] = allowed[sender][msg.sender].sub(amount);
       balances[recipient] = balances[recipient].add(amount);
-      burn();
       emit Transfer(sender, recipient, amount);
     }
 
@@ -116,11 +114,6 @@ contract Token is IERC20Token, Whitelist, Pausable {
     _;
   }
 
-  modifier sufficientBalance(address _sender, uint256 _amount) {
-    require(balances[_sender] >= _amount.add(_burnRate), "Insufficient Funds.");
-    _;
-  }
-
   modifier validRecipient(address _address) {
     require(msg.sender != _address, "Cannot send to yourself.");
     _;
@@ -128,24 +121,27 @@ contract Token is IERC20Token, Whitelist, Pausable {
 
   // BURN FUNCTIONALITIES
 
-  function burnRate() external view returns (uint256) {
-    return _burnRate;
-  }
-
   function totalBurned() external view returns (uint256) {
     return _totalBurned;
   }
-
-  function burn() internal whenNotPaused returns (bool) {
-      balances[msg.sender] = balances[msg.sender].sub(_burnRate);
-      _totalSupply = _totalSupply.sub(_burnRate);
-      _totalBurned = _totalBurned.add(_burnRate);
-      emit Burned(msg.sender, _burnRate, now);
+  
+  function addBurner(address _newBurner) external onlyOwner returns (bool) {
+    require(burners[_newBurner] == false, "Address is already a burner.");
+    burners[_newBurner] = true;
+    emit AssignedBurner(msg.sender, _newBurner, now);
   }
 
-  function changeBurnRate(
-    uint256 _amount) external onlyOwner returns (bool) {
-      _burnRate = _amount;
-      emit BurnRateChanged(msg.sender, _amount, now);
-    }
+  modifier onlyBurner() {
+    require(burners[msg.sender] == true, "Sender is not a burner.");
+    _;
+  }
+
+  function burn(
+    uint256 _burnAmount
+  ) external whenNotPaused onlyBurner returns (bool) {
+      balances[msg.sender] = balances[msg.sender].sub(_burnAmount);
+      _totalSupply = _totalSupply.sub(_burnAmount);
+      _totalBurned = _totalBurned.add(_burnAmount);
+      emit Burned(msg.sender, _burnAmount, now);
+  }
 }
